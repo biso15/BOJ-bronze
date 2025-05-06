3b665b2f46d2b4895ad9af099e08144c3a91d915
biso15 <biso150@gmail.com>
2025-05-06 09:30:07 +0900

    250506 - Day4(고양이, 개)

---
+++ src/day4/BOJ10171.java
@@ -0,0 +1,23 @@
+package day4;
+
+import java.util.Scanner;
+
+// [문제]
+// 아래 예제와 같이 고양이를 출력하시오.
+// 출력 : 고양이를 출력한다.
+// 예제 출력 :
+// \    /\
+//  )  ( ')
+// (  /  )
+//  \(__)|
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        // \    /\ 그대로 사용하면 컴파일 오류 발생
+        // "\"를 추가해서 이스케이프 처리를 해야함
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
