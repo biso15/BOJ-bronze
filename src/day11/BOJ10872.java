e607808ce6b9f08ff58681667555881e393980f7
biso15 <biso150@gmail.com>
2025-05-13 00:01:10 +0900

    250513 - Day11(1998년생인 내가 태국에서는 2541년생?!, 팩토리얼, A+B - 2)

---
+++ src/day11/BOJ10872.java
@@ -0,0 +1,25 @@
+package day11;
+
+import java.util.Scanner;
+
+// [문제]
+// 0보다 크거나 같은 정수 N이 주어진다.
+// 이때, N!을 출력하는 프로그램을 작성하시오.
+// 입력 : 첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.
+// 출력 : 첫째 줄에 N!을 출력한다.
+// 예제 입력 : 10
+// 예제 출력 : 3628800
+
+public class BOJ10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();  // 팩토리얼 : 1 ~ N까지의 곱. 단, 0!은 1
+        int result = 1;
+        if(N != 0) {
+            for(int i = 1; i <= N; i++) {
+                result *= i;
+            }
+        }
+        System.out.println(result);
+    }
+}
