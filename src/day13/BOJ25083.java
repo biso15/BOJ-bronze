62f0007244debfda598afff049bc19fc27b54afa
biso15 <biso150@gmail.com>
2025-05-15 07:09:46 +0900

    250515 - Day13(킹, 퀸, 룩, 비숍, 나이트, 폰, 새싹)

---
+++ src/day13/BOJ25083.java
@@ -0,0 +1,25 @@
+package day13;
+
+// [문제]
+// 아래 예제와 같이 새싹을 출력하시오.
+// 입력 : 입력은 없다.
+// 출력 : 새싹을 출력한다.
+// 예제 출력 :
+//          ,r'"7
+// r`-_   ,'  ,/
+//  \. ". L_r'
+//    `~\/
+//       |
+//       |
+
+public class BOJ25083 {
+    public static void main(String[] args) {
+        String sprout = "         ,r'\"7\n" + 
+                        "r`-_   ,'  ,/\n" +
+                        " \\. \". L_r'\n" +
+                        "   `~\\/\n" +
+                        "      |\n" +
+                        "      |\n";
+        System.out.println(sprout);
+    }
+}
