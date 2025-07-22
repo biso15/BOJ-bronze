0c806668464818dd085b821cc0202d11c95d6cc0
biso15 <biso150@gmail.com>
2025-07-22 22:42:59 +0900

    250722 - Day1(나머지, 단어의 개수)

---
+++ src/bronze2/day1/BOJ1152.java
@@ -0,0 +1,37 @@
+package bronze2.day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// [문제]
+// 영어 대소문자와 공백으로 이루어진 문자열이 주어진다.
+// 이 문자열에는 몇 개의 단어가 있을까?
+// 이를 구하는 프로그램을 작성하시오.
+// 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+// 입력 :
+// 첫 줄에 영어 대소문자와 공백으로 이루어진 문자열이 주어진다.
+// 이 문자열의 길이는 1,000,000을 넘지 않는다.
+// 단어는 공백 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다.
+// 또한 문자열은 공백으로 시작하거나 끝날 수 있다.
+// 출력 : 첫째 줄에 단어의 개수를 출력한다.
+// 예제 입력 : The Curious Case of Benjamin Button
+// 예제 출력 : 6
+
+public class BOJ1152 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 방법 1
+        String str = sc.nextLine().trim();
+        String[] strArr = str.split(" ");
+        System.out.println(strArr.length);
+
+        // 방법 2
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());  // 공백을 split으로 잘라서 Token에 넣음
+        System.out.println(st.countTokens());
+    }
+}
