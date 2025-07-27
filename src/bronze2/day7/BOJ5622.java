373e47f358fc8859848b6f3e909011e2bc77d1ec
biso15 <biso150@gmail.com>
2025-07-28 01:29:21 +0900

    250728 - Day7(다이얼)

---
+++ src/bronze2/day7/BOJ5622.java
@@ -0,0 +1,80 @@
+package bronze2.day7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+// [문제]
+// 상근이의 할머니는 아래 그림과 같이 오래된 다이얼 전화기를 사용한다.
+// https://u.acmicpc.net/9c88dd24-3a4c-4a09-bc50-e6496958214d/Screen%20Shot%202021-06-16%20at%2012.48.39%20AM.png
+// 전화를 걸고 싶은 번호가 있다면, 숫자를 하나를 누른 다음에 금속 핀이 있는 곳 까지 시계방향으로 돌려야 한다.
+// 숫자를 하나 누르면 다이얼이 처음 위치로 돌아가고, 다음 숫자를 누르려면 다이얼을 처음 위치에서 다시 돌려야 한다.
+// 숫자 1을 걸려면 총 2초가 필요하다.
+// 1보다 큰 수를 거는데 걸리는 시간은 이보다 더 걸리며, 한 칸 옆에 있는 숫자를 걸기 위해선 1초씩 더 걸린다.
+// 상근이의 할머니는 전화 번호를 각 숫자에 해당하는 문자로 외운다.
+// 즉, 어떤 단어를 걸 때, 각 알파벳에 해당하는 숫자를 걸면 된다.
+// 예를 들어, UNUCIC는 868242와 같다.
+// 할머니가 외운 단어가 주어졌을 때, 이 전화를 걸기 위해서 필요한 최소 시간을 구하는 프로그램을 작성하시오.
+// 입력 :
+// 첫째 줄에 알파벳 대문자로 이루어진 단어가 주어진다.
+// 단어의 길이는 2보다 크거나 같고, 15보다 작거나 같다.
+// 출력 : 첫째 줄에 다이얼을 걸기 위해서 필요한 최소 시간을 출력한다.
+// 예제 입력 : WA
+// 예제 출력 : 13
+
+public class BOJ5622 {
+    public static void main(String[] args) throws IOException {
+
+        // 방법 1
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        int time = 0;
+        for(int i = 0; i < num.length(); i++) {
+            if(num.charAt(i) < 68) {
+                time += 3;
+            } else if(num.charAt(i) < 71) {
+                time += 4;
+            } else if(num.charAt(i) < 74) {
+                time += 5;
+            } else if(num.charAt(i) < 77) {
+                time += 6;
+            } else if(num.charAt(i) < 80) {
+                time += 7;
+            } else if(num.charAt(i) < 84) {
+                time += 8;
+            } else if(num.charAt(i) < 87) {
+                time += 9;
+            } else {
+                time += 10;
+            }
+        }
+        System.out.println(time);
+
+        // 방법 2
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] charArr = br.readLine().toCharArray();
+        int sum = 0;
+        for(int i = 0; i < charArr.length; i++) {
+            char al = charArr[i];
+            if(al == 'A' || al == 'B' || al == 'C') {
+                sum += 3;
+            } else if(al == 'D' || al == 'E' || al == 'F') {
+                sum += 4;
+            } else if(al == 'G' || al == 'H' || al == 'I') {
+                sum += 5;
+            } else if(al == 'J' || al == 'K' || al == 'L') {
+                sum += 6;
+            } else if(al == 'M' || al == 'N' || al == 'O') {
+                sum += 7;
+            } else if(al == 'P' || al == 'Q' || al == 'R' || al == 'S') {
+                sum += 8;
+            } else if(al == 'T' || al == 'U' || al == 'V') {
+                sum += 9;
+            } else if(al == 'W' || al == 'X' || al == 'Y') {
+                sum += 10;
+            }
+        }
+        System.out.println(sum);
+    }
+}
