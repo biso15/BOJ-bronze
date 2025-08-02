847cd2858984cc3d478361ff9820e1b6f9cd8c5f
biso15 <biso150@gmail.com>
2025-08-02 09:15:39 +0900

    250802 - Day12(음계)

---
+++ src/bronze2/day12/BOJ2920.java
@@ -0,0 +1,78 @@
+package bronze2.day12;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+// [문제]
+// 다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다.
+// 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다.
+// c는 1로, d는 2로, ..., C를 8로 바꾼다.
+// 1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
+// 연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오.
+// 입력 :
+// 첫째 줄에 8개 숫자가 주어진다.
+// 이 숫자는 문제 설명에서 설명한 음이며, 1부터 8까지 숫자가 한 번씩 등장한다.
+// 출력 : 첫째 줄에 ascending, descending, mixed 중 하나를 출력한다.
+// 예제 입력 : 1 2 3 4 5 6 7 8
+// 예제 출력 : ascending
+
+public class BOJ2920 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        // 방법 1
+        int[] C = new int[9];
+        for(int i = 1; i <= 8; i++) {
+            C[i] = sc.nextInt();
+        }
+        int A = 0;
+        int D = 0;
+        for(int i = 1; i <= 8; i++) {
+            if(C[i] == i) {
+                A++;
+            }
+            int j = 9;
+            if(C[i] == j - i) {
+                D++;
+            }
+        }
+        if(A == 8) {
+            System.out.println("ascending");
+        } else if(D == 8) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+
+        // 방법 2
+        int N = sc.nextInt();
+        boolean flag = true;
+        if(N == 1) {
+            for(int i = 2; i <= 8; i++) {
+                int num = sc.nextInt();
+                if(num != i) {
+                    flag = false;
+                    System.out.println("mixed");
+                    break;
+                }
+            }
+            if(flag) {
+                System.out.println("ascending");
+            }
+        } else if(N == 8) {
+            for(int i = 7; i >= 1; i--) {
+                int num = sc.nextInt();
+                if(num != i) {
+                    flag = false;
+                    System.out.println("mixed");
+                    break;
+                }
+            }
+            if(flag) {
+                System.out.println("descending");
+            }
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
