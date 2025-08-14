5779336a0c655b3c6e28872ead1b29848fac0495
biso15 <biso150@gmail.com>
2025-08-14 23:31:58 +0900

    250814 - Day24(별 찍기 - 21)

---
+++ src/bronze2/day24/BOJ10996.java
@@ -0,0 +1,41 @@
+package bronze2.day24;
+
+import java.util.Scanner;
+
+// [문제]
+// 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+// 입력 : 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+// 출력 : 첫째 줄부터 차례대로 별을 출력한다.
+// 예제 입력 : 3
+// 예제 출력 :
+// * *
+//  *
+// * *
+//  *
+// * *
+//  *
+
+public class BOJ10996 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            for(int j = 1; j <= N; j++) {
+                if(j % 2 == 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+            for(int j = 1; j <= N; j++) {
+                if(j % 2 == 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
