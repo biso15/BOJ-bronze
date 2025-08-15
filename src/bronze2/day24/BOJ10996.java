2eabb6599ee0a22f3359fcc1781bd8539084d93d
biso15 <biso150@gmail.com>
2025-08-15 21:19:19 +0900

    250815 - Day25(나누기)

--- src/bronze2/day24/BOJ10996.java
+++ src/bronze2/day24/BOJ10996.java
@@ -18,6 +18,8 @@ import java.util.Scanner;
 public class BOJ10996 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 방법 1
         int N = sc.nextInt();
         for(int i = 0; i < N; i++) {
             for(int j = 1; j <= N; j++) {
@@ -33,6 +35,19 @@ public class BOJ10996 {
                     System.out.print(" ");
                 } else {
                     System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        // 방법 2
+        N = sc.nextInt();
+        for(int i = 0; i < 2 * N; i++) {
+            for(int j = 0; j < N; j++) {
+                if((i + j) % 2 == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
                 }
             }
             System.out.println();
